b25f3c3cae0665ac7bc2db26ef2be33637e3003e
Amal-Sher Kurbanov <amalsher.kurbanov@outlook.com>
2021-06-11 13:10:06 +0300

    Enable NRT and code analysis

--- AuditLog.API/Program.cs
+++ AuditLog.API/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace AuditLog.API
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
